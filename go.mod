002f147347b6c9e17505754432d862d4920a9f36
Venkata Satyanarayana Reddy Karri <venkatasatya9@gmail.com>
2020-06-26 20:41:51 +0530

    Test commits

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module satya.com/user/hello
 
 go 1.14
+
+require github.com/google/go-cmp v0.5.0
--- hello.go
+++ hello.go
@@ -3,8 +3,10 @@ package main;
 import (
 		"fmt"
 		"satya.com/user/hello/morestrings"
+		"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Printf(morestrings.ReverseRunes("!oG, olleH"))
+	fmt.Printf(cmp.Diff("Hello World", "Hello Go"))
 }
